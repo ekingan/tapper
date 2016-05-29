d917abf101265f495ce157983286b494c30dc976
ekingan <ekingan@gmail.com>
2016-05-28 21:53:08 -0700

    initial commit

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  ViewController.swift
 //  tapper
 //
@@ -9,17 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //properties
+    
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onTapBtnTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPlayButtonClick(sender: UIButton!) {
+        
+        if howManyTapsText.text != nil && howManyTapsText.text != "" {
+            
+            logoImg.hidden = true
+            playButton.hidden = true
+            howManyTapsText.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsText.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+        }
+        
     }
-
-
+    
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) taps"
+        
+    }
+    
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsText.text = ""
+        
+        logoImg.hidden = false
+        playButton.hidden = false
+        howManyTapsText.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+    }
 }
 
